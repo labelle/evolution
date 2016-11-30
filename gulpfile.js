2c4cb5d2a1d6febc770e87d4412520fd0caa1af7
jdickan <jasen@humanig.com>
2016-11-30 16:36:03 -0500

    fixed gulp pipes and tasks. evo now produces one master css file, and several split files

--- gulpfile.js
+++ gulpfile.js
@@ -40,8 +40,39 @@ gulp.task('scripts', function() {
 });
 
 /* Sass task */
-gulp.task('sass', function () {  
-    return gulp.src('scss/evolution.scss')
+gulp.task('evolution', function () {  
+    return gulp.src('scss/evo.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        includePaths: ['scss'].concat(neat)
+    }))
+    .pipe(gulp.dest('docs/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('docs/css'))
+    /* Reload the browser CSS after every change */
+    .pipe(reload({stream:true}));
+});
+
+/* Sass task */
+gulp.task('reset', function () {  
+    return gulp.src('scss/evo-reset.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        includePaths: ['scss'].concat(neat)
+    }))
+    .pipe(gulp.dest('docs/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('docs/css'))
+    /* Reload the browser CSS after every change */
+    .pipe(reload({stream:true}));
+});
+
+
+/* Sass task */
+gulp.task('elements', function () {  
+    return gulp.src('scss/evo-elements.scss')
     .pipe(plumber())
     .pipe(sass({
         includePaths: ['scss'].concat(neat)
@@ -76,9 +107,9 @@ gulp.task('browser-sync', function() {
 });
 
 /* Watch scss, js and html files, doing different things with each. */
-gulp.task('default', ['sass', 'browser-sync'], function () {
+gulp.task('default', ['evolution', 'elements', 'reset', 'scripts', 'browser-sync'], function () {
     /* Watch scss, run the sass task on change. */
-    gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['sass'])
+    gulp.watch(['scss/*.scss', 'scss/**/*.scss'], ['default'])
     /* Watch app.js file, run the scripts task on change. */
     gulp.watch(['docs/js/app.js'], ['scripts'])
     /* Watch .html files, run the bs-reload task on change. */
