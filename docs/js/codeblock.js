0036f6310c804b1dbd72c47db274bdc61a509ea4
jdickan <jasen@humanig.com>
2017-03-07 02:16:40 -0500

    design ui toolkit

---
+++ docs/js/codeblock.js
@@ -0,0 +1,38 @@
+// https://github.com/labelle/evo-code-block
+
+$(document).ready(function() {
+    // Grabs all the blocks
+    var codeBlocks = document.getElementsByClassName("evo-codeblock");
+    // Checks for blocks
+    if (codeBlocks.length > 0 ) {
+        // Loops through each block
+        $('.evo-codeblock').each(function() {
+            // Grabs the identifier based on being first in order.
+            var identifier = $(this).attr('data-identifier');
+            // Get the html for the div with matching identifier
+            var html = $('[data-identifier=' + identifier + ']').html();
+            // Fills the html in the target.
+            $('[data-target=' + identifier + '] pre code').text(html);
+        });
+    }
+
+    // Hides or reveals the code block 
+    $('.evo-multiline-reveal-button').on('click', function() {
+        // This gets the hidden code block in the same div as the button
+        var block = $(this).parent('div').find('.hidden-block');
+        // This toggles between hidding and showing the box.
+        if( block.hasClass('hidden-block') )  {
+            // Show the box.
+            $(this).parent('div').find('.hidden-block').removeClass('hidden-block').addClass('revealed');
+            $(this).text('Hide code');
+            if( $(this).hasClass('evo-remove-this')) {
+                $(this).remove();
+            }
+        } else {
+            // Hide the box.
+            $(this).parent('div').find('.revealed').removeClass('revealed').addClass('hidden-block');
+            $(this).text('View code');
+        }
+    });
+
+});
